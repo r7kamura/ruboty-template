7fa1bfed4ccbead0af29e2176d25377bdbe5af43
whywaita <whywaita@whywrite.it>
2017-02-15 22:47:36 +0900

    fix invaild path

--- heroku_exec.sh
+++ heroku_exec.sh
@@ -1,6 +1,6 @@
 /app/vendor/mecab/libexec/mecab/mecab-dict-index -d /app/vendor/mecab/lib/mecab/dic/ipadic -u original.dic -f utf-8 -t utf-8 dic.csv
 mv original.dic /app/vendor/mecab/lib/mecab/dic/ipadic/
 
-echo "userdic = /app/vendor/mecab/dic/original.dic" >> /app/vendor/mecab/lib/mecab/dic/ipadic/dicrc
+echo "userdic = /app/vendor/mecab/lib/mecab/dic/ipadic/original.dic" >> /app/vendor/mecab/lib/mecab/dic/ipadic/dicrc
 
-bundle exec ruboty
+/app/vendor/bundle/ruby/2.4.0/bin/bundle exec ruboty
