1c69f642966120365051b49f7f776ab887c1805b
whywaita <whywaita@whywrite.it>
2017-02-15 22:18:51 +0900

    use user dictionary, use toshi_a

---
+++ heroku_exec.sh
@@ -0,0 +1,7 @@
+/app/vendor/mecab/libexec/mecab-dict-index -u original.dic -f utf-8 -t utf-8 dic.csv
+mkdir -p /app/vendor/mecab/dic/
+mv original.dic /app/vendor/mecab/dic/
+
+echo "userdic = /app/vendor/mecab/dic/original.dic" >> /app/vendor/mecab/etc/mecabrc
+
+bundle exec ruboty
